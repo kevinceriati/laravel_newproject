69b5111bcbff884af73755af11f1b136740a24c2
Kevin <kevin.ceriati@le-campus-numerique.fr>
2019-02-13 11:47:58 +0100

    Phrase d'acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Salut Connard
                 </div>
 
                 <div class="links">
